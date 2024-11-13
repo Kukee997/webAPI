1724eafe4f336083b240c9d58e5701d954031eb8
Kukee997 <amaralfani@students.amikom.ac.id>
2024-11-14 06:04:01 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from mysql.connector import Error
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql://project_trickbarn:fd2ef2b891eb89b36ad8c79d3254c38237e37bcb@vjct6.h.filess.io:3307/project_trickbarn'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://project_trickbarn:fd2ef2b891eb89b36ad8c79d3254c38237e37bcb@vjct6.h.filess.io:3307/project_trickbarn'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
