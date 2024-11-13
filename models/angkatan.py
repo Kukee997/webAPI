20a990e3ee4a973dad96ee7ee3781cfa8603625f
Kukee997 <amaralfani@students.amikom.ac.id>
2024-11-14 04:23:47 +0700

    Update angkatan.py

--- models/angkatan.py
+++ models/angkatan.py
@@ -4,6 +4,4 @@ class Angkatan(db.Model):
     __tablename__ = 'angkatan'
     id = db.Column(db.Integer, primary_key=True)
     tahun = db.Column(db.String(4), nullable=False)
-
-    # Definisikan relationship
     mahasiswa = db.relationship('Mahasiswa', back_populates='angkatan')
