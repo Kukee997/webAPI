d9533f9b5b9d5a30dd5de5b3766f24b2e1ce8452
Kukee997 <amaralfani@students.amikom.ac.id>
2024-11-14 08:21:02 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,3 @@
-import mysql.connector
-from mysql.connector import Error
-import os
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://project_trickbarn:fd2ef2b891eb89b36ad8c79d3254c38237e37bcb@vjct6.h.filess.io:3307/project_trickbarn'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://"username":"password"@localhost:3306/db_flask_api'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
