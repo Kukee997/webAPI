b94e4bd9e061911660a38ba7e5f50b0f86bf11f9
Kukee997 <amaralfani@students.amikom.ac.id>
2024-11-14 09:43:36 +0700

    Update mahasiswa_controller.py

--- controllers/mahasiswa_controller.py
+++ controllers/mahasiswa_controller.py
@@ -14,19 +14,19 @@ def handle_mahasiswa():
                 'nama': m.nama,
                 'nim': m.nim,
                 'angkatan': m.angkatan.tahun if m.angkatan else None,
-                'program_studi': m.program_studi.program_studi if m.program_studi else None
+                'program_studi': m.program_studi.program_studi if m.program_studi else None  # Pastikan ini sesuai
             }
             for m in mahasiswa_list
         ]
         return jsonify(result)
-    
+
     if request.method == 'POST':
         data = request.json
         new_mahasiswa = Mahasiswa(
             nama=data['nama'],
             nim=data['nim'],
             angkatan_id=data['angkatan_id'],
-            program_studi_id=data['program_studi_id']
+            program_studi_id=data['program_studi_id']  # Pastikan ini sesuai
         )
         db.session.add(new_mahasiswa)
         db.session.commit()
@@ -39,26 +39,28 @@ def handle_mahasiswa():
             }
         }), 201
 
-@mahasiswa_bp.route('/mahasiswa/view', methods=['GET'])
-def view_mahasiswa():
-    mahasiswa_list = Mahasiswa.query.all()
-    
-    # Mengirimkan data mahasiswa ke template
+@mahasiswa_bp.route('/', methods=['GET'])
+def home():
+    mahasiswa_list = Mahasiswa.query.all()  # Ambil semua data mahasiswa
     return render_template('mahasiswa.html', mahasiswa=mahasiswa_list)
+
+
+
+
 
 @mahasiswa_bp.route('/mahasiswa/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def handle_mahasiswa_by_id(id):
     mahasiswa = Mahasiswa.query.get_or_404(id)
-    
+
     if request.method == 'GET':
         return jsonify({
             'id': mahasiswa.id,
             'nama': mahasiswa.nama,
             'nim': mahasiswa.nim,
             'angkatan': mahasiswa.angkatan.tahun if mahasiswa.angkatan else None,
-            'program_studi': mahasiswa.program_studi.program_studi if mahasiswa.program_studi else None
+            'program_studi': mahasiswa.program_studi.program_studi if mahasiswa.program_studi else None  # Pastikan ini sesuai
         })
-    
+
     if request.method == 'PUT':
         data = request.json
         mahasiswa.nama = data.get('nama', mahasiswa.nama)
@@ -67,7 +69,7 @@ def handle_mahasiswa_by_id(id):
         mahasiswa.program_studi_id = data.get('program_studi_id', mahasiswa.program_studi_id)
         db.session.commit()
         return jsonify({'message': 'Data mahasiswa berhasil diperbarui'})
-    
+
     if request.method == 'DELETE':
         db.session.delete(mahasiswa)
         db.session.commit()
